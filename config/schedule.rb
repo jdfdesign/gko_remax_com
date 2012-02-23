56d3e6b5e94a0f85b9a769cc951c86780e2b8736
jdfdesign <regis@joufdesign.com>
2012-02-23 13:55:14 -0430

    bump

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,8 +1,8 @@
 #whenever --set environment=development --update-crontab
-set :environment, "development"
-case @environment
-when 'development'
-  every 1.minute do
-    rake "rates:update"
-  end
-end
+#set :environment, "development"
+#case @environment
+#when 'development'
+#  every 1.minute do
+#    rake "rates:update"
+#  end
+#end
