ee34e75d85f6ae5870bfb0def4cfba2c0dd97aa3
regis bruggheman <regis@joufdesign.com>
2013-09-17 07:58:07 +0200

    bump

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,7 @@ module GkoRemaxStbarthCom
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.precompile += %w( modernizr-2.6.2-respond-1.1.0.min.js )
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
