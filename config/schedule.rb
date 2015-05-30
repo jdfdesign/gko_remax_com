1fa50ee1186bd4baeec08295579ef89bfbf7f114
Regis Bruggheman <regis@joufdesign.com>
2015-05-30 18:04:13 +0200

    bump

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,7 +2,8 @@ set :environment, "production"
 case @environment
 when 'production'
   every 1.hour do
-    bundle exec rake "rates:update"
+    rake "rates:update"
+    #bundle exec rake "rates:update"
   end
 end
 
